11d8698e69855c0822ecadba752432e5d4af7d0e
wuhang <wuhang@megvii.com>
2021-03-05 16:38:53 +0800

    try to optimize

--- convex_hull_stern_brocot/spoj_AFS3.cpp
+++ convex_hull_stern_brocot/spoj_AFS3.cpp
@@ -3,7 +3,9 @@
 #include <cstdio>
 #include <functional>
 #include <utility>
+#include <algorithm>
 #include <vector>
+#include <chrono>
 
 const double eps = 1e-9;
 std::pair<long long, long long>
@@ -241,14 +243,14 @@ convex_hull(const long long &N, const long long &x1, const long long &y1,
           x += right.first;
           y += sign * right.second;
           if (x < x2) {
-            //assigned = true;
-            //last = std::make_pair(x, y);
-            if (y >= 0) ret.emplace_back(x, y);
+            assigned = true;
+            last = std::make_pair(x, y);
+            //if (y >= 0) ret.emplace_back(x, y);
           } else
             return first_slope;
         }
-        //if (assigned && last.second >= 0)
-        //  ret.emplace_back(last);
+        if (assigned && last.second >= 0)
+          ret.emplace_back(last);
         left = right;
         while (!stac.empty()) {
           right = stac.back();
@@ -380,13 +382,13 @@ inline __int128 S1(__int128 x){return (x&1)?((x+1)/2*x):(x/2*(x+1));}
 inline __int128 S2(__int128 x){__int128 a=S1(x),b=2*x+1;return (a%3)?(b/3*a):(a/3*b);}
 inline __int128 Sqr(__int128 x){return x*x;}
 struct node{
-	__int128 f,g,h;
+	__int128_t f,g,h;
 	node(){f=0,g=0,h=0;}
 };
-node calc(__int128 a,__int128 b,__int128 c,__int128 n){
+node calc(__int128_t a,__int128_t b,__int128_t c,__int128_t n){
 	node ans,res;
-	__int128 m,t1,t2,s1,s2;
 	if(!n){ans.f=b/c;ans.g=Sqr(b/c);return ans;}
+  __int128_t m,t1,t2,s1,s2;
 	if(!a){
 		t1=b/c;
 		ans.f=(n+1)*t1;
@@ -394,6 +396,12 @@ node calc(__int128 a,__int128 b,__int128 c,__int128 n){
 		ans.h=S1(n)*t1;
 		return ans;
 	}
+	if (c==1) {
+    ans.f=a*S1(n)+b*(n+1);
+    ans.g=a*a*S2(n)+2*a*b*S1(n)+b*b*(n+1);
+    ans.h=a*S2(n)+b*S1(n);
+    return ans;
+  }
 	if(a>=c||b>=c){
 		t1=a/c;t2=b/c;
 		res=calc(a%c,b%c,c,n);
@@ -405,28 +413,34 @@ node calc(__int128 a,__int128 b,__int128 c,__int128 n){
 	}
 	m=(n*a+b)/c-1;
 	res=calc(c,c-b-1,a,m);
-	__int128 w1=n*(m+1),w2=n*(n+1),w3=m+1;
+	__int128_t w1=n*(m+1),w2=n*(n+1),w3=m+1;
 	ans.f=(w1-res.f);
 	ans.g=((w1*w3)-((res.h*2+res.f)));
 	ans.h=((w2*w3)-(res.f+res.g))/2;
 	return ans;
 }
 
-__int128 T(__int128 a, __int128 b, __int128 c, __int128 n) {
+__int128_t T(long long a, __int128_t b, long long c, long long n) {
   // calculate \sum_{x=0}^{n} floor((ax+b)/c)*x where a is negative
-  /*__int128 correct = 0;
-  for (__int128 x = 0; x <= n; ++x)
-    correct = correct + (a * x + b) / c * x;*/
+  if (n < 10) {
+    __int128_t correct = 0;
+    for (long long x = 1; x <= n; ++x)
+      correct = correct + (a * x + b) / c * x;
+    return correct;
+  }
   node _node = calc(-a, b + a * n, c, n);
-  __int128 my = _node.f * n - _node.h;
-  //assert(my == correct);
+  __int128_t my = _node.f * n - _node.h;
+  /*if (my != correct) {
+    _node = calc(-a, b + a * n, c, n);
+    assert(false);
+  }*/
   return my;
 }
 
 long long gcd(long long x, long long y) {
   return y ? gcd(y, x % y) : x;
 }
-__int128 S(long long N) {
+__int128_t S(long long N) {
   if (N < 12) {
     long long ret = 0;
     for (long long i = 1; i <= N; ++i)
@@ -439,21 +453,24 @@ __int128 S(long long N) {
     return -N * 1.0 / x / x;
   };
   std::function<double(long long, long long)> outside =
-      [&](long long x, long long y) { return (__int128)x * y > (__int128)N; };
+      [&](long long x, long long y) { return (__int128_t)x * y > (__int128_t)N; };
 
   std::vector<std::pair<long long, long long>> hull;
   long long x0 = sqrtl(N + 0.5), x1 = cbrtl(N) * cbrtl(N);
   convex_hull(N, x0 + 1, N / (x0 + 1) + 1, x1, f, df, outside, hull);
-  std::vector<std::pair<long long, long long>> hull_inv;
-  for (long long x = 2; x < hull.back().second; ++x)
-    hull_inv.emplace_back(x, N / x + 1);
-  for (auto iter = hull.rbegin(); iter != hull.rend(); ++iter) {
-    std::pair<long long, long long> p = std::make_pair(iter->second, iter->first);
-    hull_inv.emplace_back(p);
-  }
-  for (long long y = hull.back().second - 1; y >= 2; --y)
-    hull.emplace_back(N / y + 1, y);
-  hull.insert(hull.begin(), hull_inv.begin(), hull_inv.end());
+  //printf("size = %d\n", (int)hull.size());
+
+  const std::pair<long long, long long> last = hull.back();
+  //std::vector<std::pair<long long, long long>> hull_inv;
+  //for (long long x = 2; x < hull.back().second; ++x)
+  //  hull_inv.emplace_back(x, N / x + 1);
+  //for (auto iter = hull.rbegin(); iter != hull.rend(); ++iter) {
+  //  std::pair<long long, long long> p = std::make_pair(iter->second, iter->first);
+  //  hull_inv.emplace_back(p);
+  //}
+  //for (long long y = hull.back().second - 1; y >= 2; --y)
+  //  hull.emplace_back(N / y + 1, y);
+  //hull.insert(hull.begin(), hull_inv.begin(), hull_inv.end());
 
   /*for (const auto &i : hull)
     printf("(%lld, %lld), ", i.first, i.second);
@@ -461,20 +478,78 @@ __int128 S(long long N) {
   for (const auto &i : hull_inv)
     printf("(%lld, %lld), ", i.first, i.second);
   printf("\n");*/
-  __int128 ret = N;
-  __int128 k = N / 2;
+  __int128_t ret = 0;
+  __int128_t k = N / 2;
   if (N & 1) ret = ret + ((k & 1) ? ((k + 1) / 2 * (3 * k + 2)) : ((3 * k + 2) / 2 * (k + 1)));
   else ret = ret + ((k & 1) ? ((3 * k + 1) / 2 * k) : (k / 2 * (3 * k + 1)));
-  for (size_t i = 0; i + 1 < hull.size(); ++i) {
-    const std::pair<long long, long long> &p1 = hull.at(i), &p2 = hull.at(i + 1);
-    long long a = p2.first - p1.first, b = p1.second - p2.second;
-    if (a == 0) continue;
-    __int128 c = (__int128)p1.first * b + (__int128)p1.second * a;
-    __int128 g = gcd(a, b);
-    __int128 val = T(-b, c, a, p2.first - 1) - T(-b, c, a, p1.first - 1) - p1.first * g -  ((g & 1) ? ((g - 1) / 2 * g) : (g / 2 * (g - 1))) * (a / g);
-    ret = ret + val;
-    //if (i % 1000000 == 0) printf("%d finished, total = %d\n", (int)i, (int)hull.size());
+  for (long long x = 1; x < last.second; ++x)
+    ret += N / x * x;
+  auto section = [&](const std::pair<long long, long long> &p1, const std::pair<long long, long long> &p2, const bool &flip=false) -> __int128_t {
+    if (!flip) {
+      __int128_t a = p2.first - p1.first, b = p1.second - p2.second;
+      if (a == 0) return 0;
+      //__int128_t c = (__int128_t)p1.first * b + (__int128_t)p1.second * a;
+      __int128_t g = gcd(a, b);
+      //__int128_t val_true = T(-b, c, a, p2.first - 1) - T(-b, c, a, p1.first - 1) - p1.first * g -  ((g & 1) ? ((g - 1) / 2 * g) : (g / 2 * (g - 1))) * (a / g);
+      __int128_t points = (__int128_t)(p2.first - p1.first) + g + p2.second + p1.second;
+      __int128_t internal = ((__int128_t)(p2.second + p1.second) * (p2.first - p1.first) - points) / 2 + 1;
+      __int128_t val2 = internal + p1.second - 1 + g;
+      __int128_t val = T(-b, (__int128_t)p1.second * a, a, a - 1) + val2 * p1.first - p1.first * g -  ((g & 1) ? ((g - 1) / 2 * g) : (g / 2 * (g - 1))) * (a / g);
+      /*if (val != val_true) {
+        assert(false);
+      }*/
+      return val;
+    } else {
+      __int128_t a = p2.second - p1.second, b = p1.first - p2.first;
+      if (a == 0) return 0;
+      //__int128_t c = (__int128_t)p1.second * b + (__int128_t)p1.first * a;
+      __int128_t g = gcd(a, b);
+      //__int128_t val_true = T(-b, c, a, p2.second - 1) - T(-b, c, a, p1.second - 1) - p1.second * g -  ((g & 1) ? ((g - 1) / 2 * g) : (g / 2 * (g - 1))) * (a / g);
+      __int128_t points = (__int128_t)(p2.second - p1.second) + g + p2.first + p1.first;
+      __int128_t internal = ((__int128_t)(p2.first + p1.first) * (p2.second - p1.second) - points) / 2 + 1;
+      __int128_t val2 = internal + p1.first - 1 + g;
+      __int128_t val = T(-b, (__int128_t)p1.first * a, a, a - 1) + val2 * p1.second - p1.second * g -  ((g & 1) ? ((g - 1) / 2 * g) : (g / 2 * (g - 1))) * (a / g);
+      /*if (val != val_true) {
+        assert(false);
+      }*/
+      return val;
+    }
+  };
+  std::pair<long long, long long> now = hull.back();
+  std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
+  std::for_each(hull.rbegin(), hull.rend(), [&](const std::pair<long long, long long> &p) -> void {
+                  ret += section(now, p, true);
+                  now = p;
+                });
+  //printf("time = %lf\n", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time).count() / 1000.0);
+
+  std::swap(now.first, now.second);
+  start_time = std::chrono::system_clock::now();
+  std::for_each(hull.begin(), hull.end(), [&](const std::pair<long long, long long> &p) -> void {
+                  ret += section(now, p);
+                  now = p;
+                });
+  //printf("time = %lf\n", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time).count() / 1000.0);
+
+  /*for (long long y = last.second - 1; y >= 2; --y) {
+    std::pair<long long, long long> p = std::make_pair(N / y + 1, y);
+    ret += section(now, p);
+    now = p;
+  }*/
+  for (long long y = 3, right = N / 2, left; y <= last.second; ++y) {
+    left = N / y;
+    if (left + 1 <= last.first) {
+      ret += ((__int128_t)right - last.first + 1) * ((__int128_t)right + last.first) / 2 * (y - 1);
+      break;
+    }
+    ret += ((__int128_t)right - left) * ((__int128_t)right + left + 1) / 2 * (y - 1);
+    right = left;
   }
+  /*for (size_t i = 0; i + 1 < hull.size(); ++i) {
+    const std::pair<long long, long long> &p1 = hull.at(i), &p2 = hull.at(i + 1);
+    ret += section(p1, p2);
+    if (i % 10000000 == 0) printf("%d finished, total = %d\n", (int)i, (int)hull.size());
+  }*/
   __uint128_t temp = (N & 1) ? (((__uint128_t)N + 1) / 2 * N) : ((__uint128_t)N / 2 * (N + 1));
   ret -= temp;
   return ret;
@@ -487,6 +562,7 @@ void print(__uint128_t x) {
 }
 
 int main() {
+  // 9223372036854775807LL
   /*for (unsigned long long n = 9223372036854775807LL; n <= 9223372036854775807LL; ++n) {
     __uint128_t correct = 0;
     unsigned long long v = sqrtl(n);
